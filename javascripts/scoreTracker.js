a2f336c5912dbfee99b3e1f95c0734e62a144da8
rmanchanda@inrhythm.com <rmanchanda@inrhythm.com>
2015-03-03 10:46:05 -0500

    ScoreTracker lib

---
+++ javascripts/scoreTracker.js
@@ -0,0 +1,48 @@
+(function () {
+    Amp.ScoreTracker = function() {
+        var utils = new Amp.Utilities();
+
+        var totalScore = 0;
+
+        function init() {
+            //init
+            console.log('init');
+        }
+
+
+        /* @method setScore
+         * @description Sets the current score
+         * @param {int} Accepts integer param to set
+         */
+
+        function setScore(int) {
+            if (!this.utils.isInteger(int)) {
+                return;
+            }
+
+            //set the score to the int val
+            totalScore = int;
+        }
+
+
+        function deductPoint(int) {
+            if (!this.utils.isInteger(int)) {
+                return;
+            }
+
+            totalScore = totalScore - int;
+        }
+
+        function getTotalScore() {
+            return totalScore;
+        }
+
+
+        return {
+            setScore: setScore,
+            deductPoint: deductPoint,
+            getTotalScore: getTotalScore,
+            init: init
+        }
+    }
+})();
